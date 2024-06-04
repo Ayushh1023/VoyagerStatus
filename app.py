851340ee3b0302ffee94c861ef5a54ae9ebe1050
Ayushh1023 <shreyashashank2310@gmail.com>
2024-06-04 10:37:14 +0530

    chrome_driver added

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-service = Service("C:/Users/sshas/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe")
+service = Service("chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.implicitly_wait(3)
 
